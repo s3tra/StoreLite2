27b842fbd14686f8ad10263b9c0bf9be079921fb
s3tra <dropemail01@pm.me>
2025-03-11 16:57:56 +0000

    Bug fixes

--- src/methods/saveData.js
+++ src/methods/saveData.js
@@ -1,5 +1,9 @@
 import fs from 'fs/promises';
+import path from 'path';
+
 import getData from './getData.js';
+
+const filePath = path.resolve('data.json');
 
 const saveData = async (data) => {
   if (!data || !data._key)
@@ -10,13 +14,20 @@ const saveData = async (data) => {
     if (existingData)
       return console.warn('Unable to save data, the key is already in use.');
 
-    const file = await fs.readFile('../data.json', 'utf-8');
+    let file;
+    try {
+      file = await fs.readFile(filePath, 'utf-8');
+    } catch (error) {
+      if (error.code == 'ENOENT')
+        file = await fs.writeFile(filePath, JSON.stringify({}));
+    }
+
     const obj = await JSON.parse(file);
 
     const nextKey = Object.keys(obj) + 1;
     obj[nextKey] = data;
 
-    await fs.writeFile('../data.json', JSON.stringify(obj));
+    await fs.writeFile(filePath, JSON.stringify(obj));
     return true;
   } catch (error) {
     console.error('Unable to save data:', error);
